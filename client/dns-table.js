6e9dbbecba8c833f4ca3686e8dda0ceae21f193c
zackattack01 <zacharykennetholson@gmail.com>
2019-02-23 15:39:07 -0500

    cleanup

--- client/dns-table.js
+++ client/dns-table.js
@@ -8,25 +8,28 @@ Template.dnsLog.helpers({
 	determineResponder: function() {
 		//console.log(this);
 		if (this.responder) {
-      return "local " + this.responder;
+			return "local " + this.responder;
 		}
-    if (this.forwarded) {
-       return this.forwarded;
-    }
-    if (this.cached) {
-       this.reply = this.cached;
-       return "local cache";
-    }
+		if (this.forwarded) {
+			return this.forwarded;
+		}
+		if (this.cached) {
+			this.reply = this.cached;
+			return "local cache";
+		}
 	},
-  styles: function() {
-    var classes = "";
+	styles: function() {
+		var classes = "";
 		if (this.reply === "0.0.0.0") {
-      classes += "blackholed ";
+			classes += "text-white bg-dark ";
 		}
-    if (this.cached) {
-       classes += "cached ";
-    }
-    return classes;
-  }
+		if (this.cached) {
+			classes += "cached ";
+		}
+		if (this.forwarded) {
+			classes += "forwarded ";
+		}
+		return classes;
+	}
 });
 
